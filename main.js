59863a8d3a3bc119188002a647a023682e8ac04a
MyDay <x.artha@yahoo.com>
2018-11-17 16:55:32 +0700

    added fiture wall-bounce

--- main.js
+++ main.js
@@ -37,51 +37,61 @@ var btnStart
 var background
 var monster
 var crosshair
+var scoreText
 var score = 0
 
 function preload() {
-    // this.load.setBaseURL(_URL)
     this.load.image('background1', 'assets/img/background/bg1.png')
     this.load.image('background2', 'assets/img/background/bg2.png')
     this.load.image('background3', 'assets/img/background/bg3.png')
     this.load.spritesheet('btnStart', 'assets/img/other/btnStart.png', 193, 71)
-    this.load.spritesheet('monster1', 'assets/img/monster/monster1.png', 225, 178)
+    this.load.spritesheet('monster1', 'assets/img/monster/monster1.png', 220, 178)
     this.load.image('crosshair', 'assets/img/other/crosshair.png')
 }
 
 function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE)
     background = game.add.image(0, 0, 'background1')
-    btnStart = game.add.button(game.world.centerX - 95, 400, 'btnStart', btnStartAction, this, 2, 1, 0)
-    monster = game.add.sprite(0, 0, 'monster1')
+    scoreText = game.add.text(32, 32, 'Score: ' + score, { font: "20px Arial", fill: "#ffffff", align: "left" });
 
+    // btnStart = game.add.button(game.world.centerX - 95, 400, 'btnStart', btnStartAction, this, 2, 1, 0)
+
+    monster = game.add.sprite(0, 0, 'monster1')
+    game.physics.enable(monster, Phaser.Physics.ARCADE)
+    monster.enableBody = true
+    monster.body.collideWorldBounds = true;
+    monster.body.bounce.set(1);
+    monster.body.velocity.x = game.rnd.integerInRange(-200, 200)
+    monster.body.velocity.y = game.rnd.integerInRange(-200, 200)
+    
+    
     
     crosshair = game.add.sprite(game.world.centerX, game.world.centerY, 'crosshair');
-    
-    game.physics.enable(monster, Phaser.Physics.ARCADE);
-    game.physics.enable(crosshair, Phaser.Physics.ARCADE);
-    monster.enableBody = true
+    game.physics.enable(crosshair, Phaser.Physics.ARCADE)
     crosshair.enableBody = true
+
 }
 
 function update() {
+
     game.physics.arcade.overlap(monster, crosshair, shotMonster, null, this)
 
     crosshair.x = game.input.x - 20
     crosshair.y = game.input.y - 20
+
 }
 
 function render() {
-    game.debug.text('Score: ' + score , 32, 32);
+    game.debug.text('Monster[X]: ' + monster.x, 32, 200)
+    game.debug.text('Monster[Y]: ' + monster.y, 32, 220)
 }
 
-function btnStartAction() {
-    
-}
 
 function shotMonster(monster) {
     if (game.input.activePointer.isDown)
     {
         monster.kill()
         score += 5
+        scoreText.text = 'Score: ' + score
     }
 }
